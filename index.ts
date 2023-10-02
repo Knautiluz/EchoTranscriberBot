bcd3a87ec307efd87f6de87acd8152fd65dd9c95
Jackson Jones <knautiluz@gmail.com>
2023-10-01 21:56:04 -0300

    feat: added permission by group id

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express'
 import { WEBHOOK_URI, PORT } from './src/config/environment'
 import { audioHandler }  from './src/handlers/services-handlers'
-
+import { ALLOWED_GROUP_IDS } from './src/config/environment'
 const app = express()
 
 app.use(express.json())
@@ -14,12 +14,21 @@ const audioRequest = async (req: Request, res: Response, next: NextFunction) => 
     const { voice, chat, message_id } = req.body.message
     
     if(voice) {
+        
         const chat_id: number = chat.id
-        const file_id: string = voice.file_id
-        const msg_id: number = message_id
-        const file_unique_id: string = voice.file_unique_id
-        const mime_type: string = voice.mime_type
-        await audioHandler(chat_id, msg_id, file_id, file_unique_id, mime_type)
+
+        if(ALLOWED_GROUP_IDS.includes(`${chat_id}`)) {
+            const file_id: string = voice.file_id
+            const msg_id: number = message_id
+            const file_unique_id: string = voice.file_unique_id
+            const mime_type: string = voice.mime_type
+            await audioHandler(chat_id, msg_id, file_id, file_unique_id, mime_type)
+            return res.send()
+        } else {
+            console.log(`Grupo de ID ${chat_id} não tem permissão para usar o bot.`)
+            return next()
+        }
+
     }
     return next()
 }
--- src/config/environment.ts
+++ src/config/environment.ts
@@ -10,6 +10,7 @@ const PORT: string = env.PORT as string
 const WEBHOOK_URI = `/webhook/${BOT_TOKEN}`
 const WEBHOOK_URL: string = SERVER_URL+WEBHOOK_URI
 const MAX_GPT_TOKENS_PER_USER: number = parseInt(env.MAX_GPT_TOKENS_PER_USER || '1200')
+const ALLOWED_GROUP_IDS: string[] = env.ALLOWED_GROUP_IDS?.split(';') || []
 
 export {
     BOT_TOKEN,
@@ -19,4 +20,5 @@ export {
     WEBHOOK_URI,
     WEBHOOK_URL,
     MAX_GPT_TOKENS_PER_USER,
+    ALLOWED_GROUP_IDS
 }
